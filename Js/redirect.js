99574e267b309c0e476d85040a3ed04af792fa2b
KennyEvenCode <campb194@csusm.edu>
2023-04-23 16:59:51 -0700

    initiated redirect.js code

---
+++ Js/redirect.js
@@ -0,0 +1,6 @@
+function redirectToCustomMap() {
+  window.location.href = "/Maps/Custom_Map/CustomMap.html";
+}
+function redirectToOnRamp() {
+  window.location.href = "/Maps/On_Ramp_Map/OnRamp.html";
+}

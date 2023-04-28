ed138a1e117f025344b0e8c0ddf11e5f12e672aa
KennyEvenCode <campb194@csusm.edu>
2023-04-28 09:14:04 -0700

    organized folder structure

--- Js/redirect.js
+++
@@ -1,6 +0,0 @@
-function redirectToCustomMap() {
-  window.location.href = "/Maps/Custom_Map/CustomMap.html";
-}
-function redirectToOnRamp() {
-  window.location.href = "/Maps/On_Ramp_Map/OnRamp.html";
-}

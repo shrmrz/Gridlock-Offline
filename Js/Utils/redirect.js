971693c4dd5de32cfb329fd3af45a9bbff91832a
KennyEvenCode <campb194@csusm.edu>
2023-05-07 20:11:10 -0700

    changed href paths from absolute to relative

--- Js/Utils/redirect.js
+++ Js/Utils/redirect.js
@@ -1,39 +1,39 @@
 function redirectToCustomMap() {
-  window.location.href = "/Maps/Custom_Map/index.html";
+  window.location.href = "../../Maps/Custom_Map/index.html";
 }
 
 function pageRedirect_Ring() {
-  window.location.href = "/Maps/Ring/index.html";
+  window.location.href = "../../Maps/Ring/index.html";
 }
 
 function pageRedirect_OnRamp() {
-  window.location.href = "/Maps/On_Ramp/index.html";
+  window.location.href = "../../Maps/On_Ramp/index.html";
 }
 
 function pageRedirect_OffRamp() {
-  window.location.href = "/Maps/Off_Ramp/index.html";
+  window.location.href = "../../Maps/Off_Ramp/index.html";
 }
 
 function pageRedirect_Roadworks() {
-  window.location.href = "/Maps/Roadworks/index.html";
+  window.location.href = "../../Maps/Roadworks/index.html";
 }
 
 function pageRedirect_Uphill() {
-  window.location.href = "/Maps/Uphill/index.html";
+  window.location.href = "../../Maps/Uphill/index.html";
 }
 
 function pageRedirect_Routing() {
-  window.location.href = "/Maps/Routing/index.html";
+  window.location.href = "../../Maps/Routing/index.html";
 }
 
 function pageRedirect_Roundabout() {
-  window.location.href = "/Maps/Roundabout/index.html";
+  window.location.href = "../../Maps/Roundabout/index.html";
 }
 
 function pageRedirect_Intersection() {
-  window.location.href = "/Maps/Intersection/index.html";
+  window.location.href = "../../Maps/Intersection/index.html";
 }
 
 function pageRedirect_OnRampBaWue() {
-  window.location.href = "/Maps/OnRamp_BaWue/index.html";
+  window.location.href = "../../Maps/OnRamp_BaWue/index.html";
 }
